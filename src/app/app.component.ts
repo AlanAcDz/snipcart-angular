12a8ee61e5c7858a9cffb5469116bacd19ea75e1
Alan Acuña <alantheace@gmail.com>
2020-04-04 14:49:45 -0600

    Snipcart with Angular integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// Typescript product interface
+interface Product {
+  id: string;
+  category: string;
+  name: string;
+  price: number;
+  url: string;
+  description: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'snipcart-angular';
+export class AppComponent implements OnInit {
+  products$: Observable<Product[]>;
+  // Change to this URL: https://YOUR-FIREBASE-PROJECT.firebaseio.com/products.json
+  productsUrl = 'assets/products.json';
+  constructor(
+    private http: HttpClient
+  ) {}
+  ngOnInit() {
+    this.products$ = this.getProducts();
+  }
+  // Method that obtains the products from RTDB
+  getProducts() {
+    return this.http.get<Product[]>(this.productsUrl);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
